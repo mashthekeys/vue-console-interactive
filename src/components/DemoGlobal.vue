e6c18586a073a22d3c76bfb0f081612f650276be
Andy Holland <github@ahweb.co.uk>
2020-07-26 00:08:14 +0100

    Added demo for replacing the global console.

---
+++ src/components/DemoGlobal.vue
@@ -0,0 +1,16 @@
+<template>
+  <ConsoleOutput mount-global />
+</template>
+
+<script>
+  import ConsoleOutput from "./ConsoleOutput";
+
+  export default {
+    name: "DemoGlobal",
+    components: {ConsoleOutput}
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ConsoleInteractive from '@/components/ConsoleInteractive'
+import DemoGlobal from '@/components/DemoGlobal'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'ConsoleInteractive',
+      name: 'Echo Demo',
       component: ConsoleInteractive
+    },
+    {
+      path: '/global',
+      name: 'Global Console Demo',
+      component: DemoGlobal
     }
   ]
 })
